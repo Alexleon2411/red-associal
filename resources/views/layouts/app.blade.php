9ea09dceab5a176885f46167c7789240bce175ee
alejandro leon <alejandroleon1124@gmail.com>
2025-06-08 21:08:27 +0200

    new work for profile dashboard and messages boards done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @stack('styles')
-        <title>Login app PHP</title>
+        <title>Red-Associal</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
